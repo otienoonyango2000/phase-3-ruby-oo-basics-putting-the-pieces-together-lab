b3368f47e2d3d5ec232ed3ea60773407c717e096
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-02-12 14:31:53 +0300

    putting the pieces together first commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    
+    attr_accessor:title,:author, :page_count, :genre
+    def initialize (title= "And Then There Were None")
+        @title=title  
+        
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+    
+
+end
+book = Book.new("The World According to Garp")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
+
 
 
